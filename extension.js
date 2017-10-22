7a0cda07b1d8740b38a12110ffb34b322e3090d6
Erik P. Hansen <eph@erikphansen.com>
2017-10-22 16:33:30 -0700

    Working draft

--- extension.js
+++ extension.js
@@ -1,30 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+function convertToColumnSelection(editor, selection) {
+  let startPosition = selection.start;
+  let endPosition = selection.end;
+  let newSelections = [];
+  let linesInSelection = endPosition.line - startPosition.line + 1;
+  if (linesInSelection < 2) {
+    return;
+  }
+  // Create a new selection for each line that the currentSelection spans
+  for (let i = 0; i < linesInSelection; i++) {
+    const thisLine = startPosition.line + i;
+    newSelections.push(
+      new vscode.Selection(
+        thisLine,
+        startPosition.character,
+        thisLine,
+        endPosition.character
+      )
+    );
+  }
+  editor.selections = newSelections;
+}
+
+function convertToContiguousSelection(editor, selections) {
+  // get the `start` of the first selection
+  const startPosition = selections[0].start;
+  // get the `end` of the last selection
+  const endPosition = selections[selections.length - 1].end;
+  editor.selection = new vscode.Selection(startPosition, endPosition);
+}
+
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "toggle-column-selection" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand(
+    'extension.toggleColumnSelection',
+    function() {
+      const editor = vscode.window.activeTextEditor;
+      const selections = editor.selections;
+      if (!!!selections.length) {
+        return;
+      } else if (selections.length > 1) {
+        convertToContiguousSelection(editor, selections);
+      } else {
+        convertToColumnSelection(editor, selections[0]);
+      }
+    }
+  );
+  context.subscriptions.push(disposable);
 }
+
+function deactivate() {}
+
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
 exports.deactivate = deactivate;
